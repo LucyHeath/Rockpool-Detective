9dd4a93d4c820a254df0f33a562d9881ca7d1d6e
Lucy Heath <lucyeheath@outlook.com>
2022-10-18 13:55:25 +0100

    blackClue function working

--- scripts/app.js
+++ scripts/app.js
@@ -15,11 +15,6 @@ function init() {
   // numWhiteClues
   // Score
 
-  // let startingCells = [20, 21, 22, 23]
-  // startingCells.forEach((cell) => {
-  //   console.log(decodingCells[cell])
-  // })
-  // let currentCell = startingCells
   const choiceArray = [
     'starfish',
     'urchin',
@@ -39,7 +34,7 @@ function init() {
 
   // ? Execution
 
-  //  Step 1- loop through cells , make an array of arrays with 4 cells in each array
+  //  loop through cells , make an array of arrays with 4 cells in each array, 6 arrays total (0-5)
   for (let i = 0; i < decodingCells.length; i++) {
     row.push(i)
     if (row.length >= 4) {
@@ -48,14 +43,23 @@ function init() {
     }
   }
 
-  //Update
+  let currentRow = rowsArray.length - 1
+  console.log(currentRow)
+
   function updateDOMRow(choiceArr, rowArr) {
+    //loop through player choice and row array,
     for (let i = 0; i < choiceArr.length; i++) {
+      //update decoding cells with player choice- index of player choice matches index of cell it should appear in
       decodingCells[rowArr[i]].classList.add(choiceArr[i])
     }
-    console.log(choiceArr)
-    console.log(rowArr)
   }
+
+  function nextRow() {
+    // funciton to fill up next row on grid once first row full- called at end of each round of the game
+    currentRow--
+  }
+  nextRow()
+  console.log(currentRow)
 
   function playerChoice(event) {
     // * clicking the choices pushes value into playerCurrentChoiceArray , until it holds 4 values
@@ -64,9 +68,10 @@ function init() {
       console.log(playerCurrentChoiceArray)
     }
     // player choice displayed in decoding grid cell
-    updateDOMRow(playerCurrentChoiceArray, rowsArray[5])
+    updateDOMRow(playerCurrentChoiceArray, rowsArray[currentRow])
+    compareChoiceArrays()
 
-    // continue looping click and adding to grid fo all 4 choices
+    // move to next row once full
   }
 
   function computerRandomChoice() {
@@ -76,15 +81,46 @@ function init() {
         choiceArray[Math.floor(Math.random() * choiceArray.length)]
       computerCurrentChoiceArray.push(randomChoice)
       console.log(computerCurrentChoiceArray)
-      console.log(randomChoice)
     }
   }
-
   computerRandomChoice()
 
-  function playGame() {}
-  function resetGame() {}
-  function gameOver() {}
+  // const arr1 = ['hey', 'hello', 'hi']
+  // const arr2 = ['goodbye', 'hi', 'hey']
+
+  function compareChoiceArrays() {
+    //full  matches- save direct matches from both arrays to new variable, which is an array called fullMatches
+    let fullMatches = computerCurrentChoiceArray.filter((element) =>
+      playerCurrentChoiceArray.includes(element)
+    )
+    console.log(fullMatches)
+    //save no of items in fullMatch array to numBlackKeys variable
+    let numblackClues = fullMatches.length
+    console.log(numblackClues)
+  }
+  compareChoiceArrays()
+
+  //partial matches
+  // Problem- if same item selected multiple times by player will count as a partial match
+
+  // Need keep track of the partial matches as through in partialMatches variable
+  // Loop through existing values in partial matches array
+
+  // Each time we check for a potential match, we should first check the previous partial matches we've already found to make sure that we're not counting the same one twice
+
+  // One way to approach this would be to create a "clean" version of potential matches by removing any possible duplicates before running our match checks. That way we can be sure that if we find a match, it isn't a duplicate
+
+  // I'd recommend using a variable outside the loop to store the matches we find, and the includes method to check for a possible match
+
+  function resetGame() {
+    score = 0
+    scoreDisplay.textContent = score
+    endGameTeext.innerHTML
+  }
+
+  function endGame() {
+    alert(score)
+  }
 
   // ? Events
   //click event for player choice
--- scripts/pseudo.js
+++ scripts/pseudo.js
@@ -1,9 +1,8 @@
 function init() {
-
   // ? Elements
-  // choice 
-  // decoding grid cell 
-  // clue grid cell 
+  // choice
+  // decoding grid cell
+  // clue grid cell
 
   // play again button
   // end of game text
@@ -23,50 +22,48 @@ function init() {
 
   // ? Execution
 
-  function computerChoice(){
+  // let startingCells = [20, 21, 22, 23]
+  // startingCells.forEach((cell) => {
+  //   console.log(decodingCells[cell])
+  // })
+  // let currentCell = startingCells
+
+  function computerChoice() {
     // computer will randomly make a choice
     // * from the choicesArray using randomIndex
     // * this is pushed into computerCurrentChoiceArray
   }
 
-  function playerChoice(){
+  function playerChoice() {
     // player makes a choice
-    // * clicking the choices select from the choicesArray 
-    // * pushes this into playerCurrentChoiceArray 
-
+    // * clicking the choices select from the choicesArray
+    // * pushes this into playerCurrentChoiceArray
     // player choice displayed in decoding grid cell ( has a generic class e.g. disabled ) which can  be removed e.g 4 cells at a time
-    // * add choice to the decoding grid cell classList.add() 
-
+    // * add choice to the decoding grid cell classList.add()
     // continue looping click and adding to grid, until all available cell are full for that round
-    // * each round disable the next 4 cells , until the last row reached. 
+    // * each round disable the next 4 cells , until the last row reached.
   }
 
-
-  function playGame(){
+  function playGame() {
     // the playerCurrentChoiceArray compared against the computer computerCurrentChoiceArray
-    // 1) compare both Value and position 
-    // * If a playerChoiceArray[i] = computerChoiceArray[i] &  playerChoiceArray value = computerChoiceArray value -->  add to a rightChoiceRightPlaceArray 
-    // this is repeated for the array.length 
-    //// let full matches = playerChoiceArray[i] = computerChoiceArray[i] &  playerChoiceArray value = computerChoiceArray value
-    ////if...loop through ...
-    //// remove full matches- 
-
-    //// let partial matches
-    //// for loop - index to match 
-
-    // 2) Compare only Value  
-    // * Then if playerChoiceArray[value] = computerChoiceArray[value]and playerChoiceArray[i] != computerChoiceArray[i]-->  save to rightChoiceWrongPlaceArray 
-    // this is repeated for the array.length 
-
+    // 1) compare both Value and position
+    // * If a playerChoiceArray[i] = computerChoiceArray[i] &  playerChoiceArray value = computerChoiceArray value -->  add to a rightChoiceRightPlaceArray
+    // this is repeated for the array.length
+    // let full matches = playerChoiceArray[i] = computerChoiceArray[i] &  playerChoiceArray value = computerChoiceArray value
+    //if...loop through ...
+    // remove full matches-
+    // let partial matches
+    // for loop - index to match
+    // 2) Compare only Value
+    // * Then if playerChoiceArray[value] = computerChoiceArray[value]and playerChoiceArray[i] != computerChoiceArray[i]-->  save to rightChoiceWrongPlaceArray
+    // this is repeated for the array.length
     // 3) Then add clues
     // * rightChoiceRightPlace[i].lenght + 1  =  number of black clue keys --> saved to blackKeysArray and added as class to clue grid cell - classList.add()
     // * rightChoiceWrongPlace[i].lenght +1  =  number of white clue keys to be printed --> saved to whiteKeysArray and added as class to clue grid cell - classList.add()
-
     // playGame() should loop through until no spaces left on grid;
     //  * if playerChoiceArray != computerChoiceArray and  number of rows in decoding grid is less than or = to (no of rows)6   - keep playing
     //  * if playerChoiceArray != computerChoiceArray and number of rows in decoding grid is greater than (no of rows)6 - computer wins - gameover()
     //  * if playerChoiceArray = computerChoiceArray and  number of rows in decoding grid is less than or = to (no of rows) 6 - player wins- gameover()
-
     // ! playGame() should loop through until condition met
     // ! * numberOfAttempts =< 6 playerChoiceArray != computerChoiceArray - keep playing
     // ! * numberOfAttempts = 6 & playerChoiceArray != computerChoiceArray - computer wins
@@ -74,24 +71,24 @@ function init() {
     // ! * numberOfAttempts =< 6 & playerChoiceArray = computerChoiceArray - player wins
   }
 
-  function resetGame(){
-  // Can be called at any time, when play again button is clicked
-  // Resets the score and display to 0
-  // Resets the decoding grid to clear
-  // Resets the clue grid to clear
-  // can be done at any time during the game
+  function resetGame() {
+    // Can be called at any time, when play again button is clicked
+    // Resets the score and display to 0
+    // Resets the decoding grid to clear
+    // Resets the clue grid to clear
+    // can be done at any time during the game
   }
 
-  function gameOver(){
+  function gameOver() {
     // End of game update the score display innerHTML
     // * if computer wins , display message
     // * if player wins , display score and message
   }
 
-  // ? Events 
+  // ? Events
   // click on choice
-  //* Event listner listens for player click on all items in choice array 
-  
+  //* Event listner listens for player click on all items in choice array
+
   // click on play again button
   //* Event listner listens for player click on reset button
 }
@@ -100,46 +97,16 @@ document.addEventListener('DOMContentLoaded', init)
 
 // First remove all the direct matches from both arrays, and count how many full matches you're removing and save to a variable
 
-  // Then we check for partials
+// Then we check for partials
 
-  // We want to check for partial matches but if a player has selected the same colour twice, that the computer has only selected once, we don't want both to be counted as a partial match
+// We want to check for partial matches but if a player has selected the same colour twice, that the computer has only selected once, we don't want both to be counted as a partial match
 
-  // To avoid this we need to keep track of the partial matches we've found so far as we're going through
+// To avoid this we need to keep track of the partial matches we've found so far as we're going through
 
-  // Each time we check for a potential match, we should first check the previous partial matches we've already found to make sure that we're not counting the same one twice
+// Each time we check for a potential match, we should first check the previous partial matches we've already found to make sure that we're not counting the same one twice
 
-  // One way to approach this would be to create a "clean" version of potential matches by removing any possible duplicates before running our match checks. That way we can be sure that if we find a match, it isn't a duplicate
+// One way to approach this would be to create a "clean" version of potential matches by removing any possible duplicates before running our match checks. That way we can be sure that if we find a match, it isn't a duplicate
 
-  // I'd recommend using a variable outside the loop to store the matches we find, and the includes method to check for a possible match
+// I'd recommend using a variable outside the loop to store the matches we find, and the includes method to check for a possible match
 
-
-
-
-  //   const grid = document.querySelector('.grid')
-
-//   function createGrid(){
-//     const width = 4 //width of grid
-//     const height = 6 //height of grid 
-//     const cellCount = width * height
-//     for (let i = 0; i < cellCount; i++ ){
-//       const cell = document.createElement('div')
-//       cell.dataset.index = i
-//       grid.appendChild(cell)
-//     }
-// }
-// createGrid()
-
-// const clueGrid = document.querySelector('.clue-grid')
-
-//   function createClueGrid(){
-//     const width = 1 
-//     const height = 6 
-//     const cellCount = width * height
-//     for (let i = 0; i < cellCount; i++ ){
-//       const cell = document.createElement('div')
-//       cell.dataset.index = i
-//       clueGrid.appendChild(cell)
-//     }
-// }
-// createClueGrid()
-// }
+//   const grid = document.querySelector('.grid')
